17897f69597744af1b8d67e48c2bbda6245766c1
Joseph Gage <Joseph.Gage@softwire.com>
2018-11-09 17:35:53 +0000

    Create website for user input and output

---
+++ BusBoard.Api/BusStop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace BusBoard.Api
+{
+    public class BusStop
+    {
+        public List<Bus> BusList;
+        public string StopName;
+
+        public BusStop(List<Bus> busList, string stopName)
+        {
+            BusList = busList;
+            StopName = stopName;
+        }
+    }
+}
--- BusBoard.Web/Controllers/HomeController.cs
+++ BusBoard.Web/Controllers/HomeController.cs
@@ -1,38 +1,58 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using BusBoard.Api;
 using BusBoard.Web.Models;
 using BusBoard.Web.ViewModels;
 
 namespace BusBoard.Web.Controllers
 {
-  public class HomeController : Controller
-  {
-    public ActionResult Index()
+    public class HomeController : Controller
     {
-      return View();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult BusInfo(PostcodeSelection selection)
+        {
+            // Add some properties to the BusInfo view model with the data you want to render on the page.
+            // Write code here to populate the view model with info from the APIs.
+            // Then modify the view (in Views/Home/BusInfo.cshtml) to render upcoming buses.
+            var postcode = new BusInfo(selection.Postcode);
+            var coordinates = ApiFetcher.GetCoordinates(postcode.PostCode);
+
+            var stopPoints = ApiFetcher.GetStopPoints(coordinates.result);
+
+            var busStops = new List<BusStop>();
+
+            foreach (var stopPoint in stopPoints)
+            {
+                var buses = ApiFetcher.GetBuses(stopPoint);
+                foreach (var bus in buses)
+                {
+                    bus.timeToStation = bus.timeToStation / 60;
+                }
+                var busStop = new BusStop(buses, stopPoint.commonName);
+
+                busStops.Add(busStop);
+            }
+
+            return View(busStops);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Information about this site";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact us!";
+
+            return View();
+        }
     }
-
-    [HttpGet]
-    public ActionResult BusInfo(PostcodeSelection selection)
-    {
-      // Add some properties to the BusInfo view model with the data you want to render on the page.
-      // Write code here to populate the view model with info from the APIs.
-      // Then modify the view (in Views/Home/BusInfo.cshtml) to render upcoming buses.
-      var info = new BusInfo(selection.Postcode);
-      return View(info);
-    }
-
-    public ActionResult About()
-    {
-      ViewBag.Message = "Information about this site";
-
-      return View();
-    }
-
-    public ActionResult Contact()
-    {
-      ViewBag.Message = "Contact us!";
-
-      return View();
-    }
-  }
 }
